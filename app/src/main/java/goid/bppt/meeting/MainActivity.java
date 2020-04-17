0022b28e63175e53ef73017453a1c5f9c7ef793a
unknown <danypambudi@gmail.com>
2020-04-18 05:33:02 +0700

    Meeting BPPT v1.0

---
+++ app/src/main/java/goid/bppt/meeting/MainActivity.java
@@ -0,0 +1,132 @@
+package goid.bppt.meeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.widget.Toast;
+
+import com.kishan.askpermission.AskPermission;
+import com.kishan.askpermission.ErrorCallback;
+import com.kishan.askpermission.PermissionCallback;
+import com.kishan.askpermission.PermissionInterface;
+
+
+public class MainActivity extends AppCompatActivity implements PermissionCallback, ErrorCallback {
+
+    private static final int REQUEST_PERMISSIONS=20;
+    private static final int TIME_INTERVAL = 2000;
+    private long mBackPressed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        reqPermission();
+
+        // Initialize default options for Jitsi Meet conferences.
+        URL serverURL;
+        try {
+            serverURL = new URL("https://meeting.bppt.go.id");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Invalid server URL!");
+        }
+        JitsiMeetConferenceOptions defaultOptions
+            = new JitsiMeetConferenceOptions.Builder()
+                .setServerURL(serverURL)
+                .setWelcomePageEnabled(false)
+                .build();
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+    }
+
+    public void onButoonClick(View v) {
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+
+        if (text.length() > 0) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            JitsiMeetConferenceOptions options
+                = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .build();
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(this, options);
+        }
+    }
+
+    private void reqPermission() {
+        new AskPermission.Builder(this).setPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.RECORD_AUDIO)
+                .setCallback(this)
+                .setErrorCallback(this)
+                .request(REQUEST_PERMISSIONS);
+    }
+
+    @Override
+    public void onShowSettings(final PermissionInterface permissionInterface, int requestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("We need permissions for this app. Open setting screen?");
+        builder.setPositiveButton("oke", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                permissionInterface.onSettingsShown();
+            }
+        });
+        builder.setNegativeButton("cancel", null);
+        builder.show();
+    }
+
+    @Override
+    public void onShowRationalDialog(final PermissionInterface permissionInterface, int requestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("We need permissions for this app.");
+        builder.setPositiveButton("oke", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                permissionInterface.onDialogShown();
+            }
+        });
+        builder.setNegativeButton("cancel", null);
+        builder.show();
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode) {
+        Toast.makeText(this, "Permissions Received.", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode) {
+        Toast.makeText(this, "Permissions Denied.", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+            super.onBackPressed();
+            return;
+        } else {
+            Toast.makeText(getBaseContext(), "Tekan Sekali Lagi Untuk Keluar",    Toast.LENGTH_SHORT).show();
+        }
+        mBackPressed = System.currentTimeMillis();
+    }
+}
